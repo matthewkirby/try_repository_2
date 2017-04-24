55cc780e481ce9f806e94ac526e0c9c9db4f63f7
matthewkirby <matthewkirby@email.arizona.edu>
2017-04-24 15:52:13 -0700

    Added addition!

--- code.py
+++ code.py
@@ -2,3 +2,8 @@
 def do_subtract(a,b):
     c = a - b 
     print(a, ' - ', b ,' is ',c)# this will make it much easier in future problems to see that something is actually happening
+
+
+def do_add(a,b):
+    c = a+b
+    print(a, '+', b, 'is', c)
